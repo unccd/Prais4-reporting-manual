69c539f5dffc28c9c5009b624f172b8b8ad638f1
Andrei Melis <andrei.melis@eaudeweb.ro>
2021-06-15 12:20:08 +0300

    Testing show GitHub source

--- conf.py
+++ conf.py
@@ -50,3 +50,12 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+html_context = {
+    "display_github": True,
+    "github_user": "eaudeweb",
+    "github_repo": "Prais4-reporting-manual",
+    "github_version": "master",
+    "conf_py_path": "/",
+}
