215d9841c4c3641eebd873cb400dd87a2325a9d1
Andrei Melis <andrei.melis@eaudeweb.ro>
2021-10-05 17:31:52 +0300

    Fix typo in conf.py

--- conf.py
+++ conf.py
@@ -88,7 +88,7 @@ html_logo = 'img/unccd_logo.png'
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = 'imag/favicon.ico'
+html_favicon = 'img/favicon.ico'
 
 # see https://docs.readthedocs.io/en/stable/guides/pdf-non-ascii-languages.html
 latex_engine = 'xelatex'
