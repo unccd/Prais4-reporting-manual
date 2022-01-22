a9e933e31d8f66a2f1f2a4e834d77cfcb42d7533
Andrei Melis <andrei.melis@eaudeweb.ro>
2022-01-22 19:20:34 +0200

    Added locales/source/*.pot

--- make_translations.sh
+++ make_translations.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
-sphinx-build -b gettext . _build/gettext
-sphinx-intl update -p _build/gettext -l es -l fr -l ar -l ru -l zh
+sphinx-build -b gettext -d _build/doctrees . locales/source
+sphinx-intl update -p locales/source -l es -l fr -l ar -l ru -l zh
+sphinx-build -b html -d _build/doctrees -D language=es . _build/html
